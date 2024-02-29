08da695ca3369380fd88f932faab818920dac69e
jackfiled <xcrenchangjun@outlook.com>
2024-02-29 11:20:16 +0800

    20240229 Finished

--- src/problem/mod.rs
+++ src/problem/mod.rs
@@ -59,3 +59,4 @@ mod p938_range_sum_of_bst;
 mod p889_construct_binary_tree_from_preorder_and_postorder_traversal;
 mod p2867_count_valid_paths_in_a_tree;
 mod p2673_make_costs_of_paths_equal_in_a_binary_tree;
+mod p2581_count_number_of_possible_root_nodes;
