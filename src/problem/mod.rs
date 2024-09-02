828e1f910dd8269016bd7811e9b91bdf277c2a0b
jackfiled <xcrenchangjun@outlook.com>
2024-09-02 11:59:40 +0800

    20240902 finished.

--- src/problem/mod.rs
+++ src/problem/mod.rs
@@ -223,3 +223,4 @@ mod p3142_check_if_grid_satisfies_conditions;
 mod p3153_sum_of_digit_differences_of_all_pairs;
 mod p3127_make_a_square_with_the_same_color;
 mod p1450_number_of_students_doing_homework_at_a_given_time;
+mod p2024_maximize_the_confusion_of_an_exam;
