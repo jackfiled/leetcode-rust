7fd29c8f96ec03f508208771125a2db0c7b20f81
jackfiled <xcrenchangjun@outlook.com>
2025-03-28 12:19:48 +0800

    20250328 finished.

--- src/problem/mod.rs
+++ src/problem/mod.rs
@@ -564,3 +564,5 @@ mod p2711_difference_of_number_of_distinct_values_on_diagonals;
 mod p2829_determine_the_minimum_sum_of_a_k_avoiding_array;
 
 mod p2712_minimum_cost_to_make_all_characters_equal;
+
+mod p2716_minimize_string_length;
